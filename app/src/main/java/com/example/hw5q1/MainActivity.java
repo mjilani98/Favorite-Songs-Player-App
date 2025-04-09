ad2611e3f1d0e9089e8a7414d30ce8ad780aa241
mjilani98 <mjilani@emich.edu>
2025-04-09 12:43:19 -0400

    Edits on the loading the songs

--- app/src/main/java/com/example/hw5q1/MainActivity.java
+++ app/src/main/java/com/example/hw5q1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.hw5q1;
 
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private SoundPool soundPool ;   //sound pool
+    private int soundId;            //sound id
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        //display the screen
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        //create sound pool builder
+        SoundPool.Builder soundPoolBuilder = new SoundPool.Builder();
+
+        //create sound pool
+        soundPool = soundPoolBuilder.build();
+
+        //load audio file into sound pool and get sound id
+        soundPool.load(this,R.raw.lotus,1);
+        soundPool.load(this,R.raw.morning,2);
+        soundPool.load(this,R.raw.startingoutwaltzvivace,3);
+        soundPool.load(this,R.raw.funkyboxstep,4);
+        soundPool.load(this,R.raw.igotastickarrbryanteoh,5);
+    }
+    //event handler of play button
+    public void play(View view)
+    {
+        //play sound
+        soundPool.play(soundId, 1, 1, 1, 0, 1);
     }
 }
